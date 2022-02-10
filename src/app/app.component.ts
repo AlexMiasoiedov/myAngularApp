18ee4962863e906ed3a77d12e39a5b1b39ee42bd
AlexMiasoiedov <alexmiasoiedov@gmail.com>
2022-02-10 09:30:03 +0200

    inint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'myApp';
+  title: string = 'myApp';
+  startColor: string = 'white';
+  myColor: string = this.startColor;
+  counter: number = 1;
+  timeoutId: null | ReturnType<typeof setTimeout> = null;
+
+  startColorSemaphore(): void {
+    this.timeoutId = setInterval(() => {
+        this.counter = (this.counter + 1) % 2
+
+        if (this.counter == 0) { this.myColor = 'green'; }
+        else { this.myColor = 'red'; }
+      }, 2500);
+  }
+
+  stopColorSemaphore(): void {
+    if (this.timeoutId != null) {
+      clearInterval(this.timeoutId);
+    }
+  }
+
+  changeStartColor(newColor: string = this.myColor): void {
+    this.startColor = newColor;
+  }
+
+  rotateColor(): void {
+    this.stopColorSemaphore();
+    this.myColor = this.startColor;
+    this.startColorSemaphore();
+  }
+
+  constructor() {
+    this.startColorSemaphore()
+  }
 }
